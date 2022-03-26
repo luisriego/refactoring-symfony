060874c3a1f17dbe7345a2ccb9d176a3b7958dc6
luisriego <luisriego@hotmail.com>
2022-03-26 14:28:42 -0300

    user-condo relationships created

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,9 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -25,7 +28,37 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     private ?string $password;
 
-    public function getId(): ?string
+    /**
+     * The plain non-persisted password
+     */
+    private ?string $plainPassword;
+
+    #[ORM\Column]
+    private ?string $firstName;
+
+    #[ORM\Column]
+    private ?string $lastName;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $avatar;
+
+    #[ORM\Column(type: 'boolean')]
+    protected ?bool $isActive = false;
+
+    #[ORM\Column(type: 'datetime')]
+    protected \DateTime $createdOn;
+
+    #[ORM\ManyToMany(targetEntity: Condo::class, mappedBy: 'users')]
+    private $condos;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v4()->toRfc4122();
+        $this->createdOn = new \DateTime();
+        $this->condos = new ArrayCollection();
+    }
+
+    public function getId(): string
     {
         return $this->id;
     }
@@ -38,6 +71,108 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->firstName.' '.$this->lastName;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function getAvatarUrl(): ?string
+    {
+        if (!$this->avatar) {
+            return null;
+        }
+
+        if (strpos($this->avatar, '/') !== false) {
+            return $this->avatar;
+        }
+
+        return sprintf('/uploads/avatars/%s', $this->avatar);
+    }
+
+    public function setAvatar(?string $avatar): void
+    {
+        $this->avatar = $avatar;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(?bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
+    public function getCreatedOn(): \DateTime
+    {
+        return $this->createdOn;
+    }
+
+    public function setCreatedOn(\DateTime $createdOn): void
+    {
+        $this->createdOn = $createdOn;
+    }
+
+    /**
+     * @return Collection<int, Condo>
+     */
+    public function getCondos(): Collection
+    {
+        return $this->condos;
+    }
+
+    public function addCondo(Condo $condo): self
+    {
+        if (!$this->condos->contains($condo)) {
+            $this->condos[] = $condo;
+        }
+
+        return $this;
+    }
+
+    public function removeCondo(Condo $condo): self
+    {
+        $this->condos->removeElement($condo);
 
         return $this;
     }
@@ -112,5 +247,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getFullName();
     }
 }
